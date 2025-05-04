b273a7f8c74ccbbac26484f2dcb594f70348debe
FAUXON <testeintrusion@gmail.com>
2025-05-04 23:40:55 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Ajoute cette ligne pour les chemins relatifs
   resolve: {
     alias: {
-      "@": "/src",
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, "src/main.tsx"),
     },
   },
 });
