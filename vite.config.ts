443ae5f1bef477c624fa4f401bbb7b50458b792f
FAUXON <testeintrusion@gmail.com>
2025-05-04 21:10:51 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ["lucide-react"],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: "src/main.tsx",
+      external: [], // Ajoute ici si nécessaire
+    },
   },
 });
